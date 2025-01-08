a2cb62df9ce267091052732e80ea171f4537f136
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2025-01-08 14:27:42 +0100

    A start of clear
    I started by just changing the colors and have now realized that the numbers also change. Therefor I need to remake the code.

--- clear/index.js
+++ clear/index.js
@@ -1,1 +1,52 @@
-document.title = "Numbers::clear";
+document.title = "Numbers:: Clear";
+
+let theButton = document.getElementById("fillClear");
+
+
+function clear(){
+    const boxes = document.querySelectorAll('.numberBox');
+
+    for(let box of boxes){
+
+        box.addEventListener("mouseover", function(){
+
+            if(box.style.color != "red" && box.style.color != "orange"){
+
+                box.style.backgroundColor = "blue";
+
+            };
+
+            box.addEventListener("click", function(){
+
+                box.style.color = "red";
+                box.style.backgroundColor = "red";
+            });
+        });
+
+        box.addEventListener("mouseout", function(){
+            if(box.style.color == "red"){
+                box.style.backgroundColor = "orange";
+                box.style.color = "orange";
+            }
+
+            if(box.style.color != "orange"){
+                box.style.backgroundColor ="lightgray";
+            }
+        })
+    }
+
+    theButton.addEventListener("click", function(){
+
+        for(let box of boxes){
+
+            box.style.backgroundColor = "lightgray";
+            box.style.color = "black";
+        }
+    })
+}
+
+clear();
+
+numButton.addEventListener("click", function(){
+    clear();
+});
