0694cb86664ec26b4293bc890fab743ce3009cd9
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2025-01-08 20:02:29 +0100

    Find same finished
    Simple code with add event listener that first holds the number clicked in a seperate variable. Then it will go in to another loop where it checks if it is the same as any other. If so the counter will be affected, it will get affected as you click because there will always be at least one number. Depending on the counter it gives two ways of text (preference by me). Then a very easy code to restore the background colors and text.

--- findSame/index.js
+++ findSame/index.js
@@ -1,1 +1,69 @@
-document.title = "Numbers::findSame";
+document.title = "Numbers:: Find Same";
+
+let theResult = document.getElementById("result");
+
+let resetButton = document.getElementById("reset");
+
+function findNumber(){
+
+    const boxes = document.querySelectorAll('.numberBox');
+
+    let clickedNumber = 0;
+
+
+    for(let box of boxes){
+
+        let counter = 0;
+
+        box.addEventListener("click", function(){
+
+            clickedNumber = parseFloat(box.textContent);
+
+            for(let box of boxes){
+
+                if(clickedNumber == parseFloat(box.textContent)){
+
+                    box.style.backgroundColor = "lightgreen";
+                    counter += 1;
+
+                }
+
+                if(clickedNumber != parseFloat(box.textContent)){
+
+                    box.style.backgroundColor = "lightgray";
+                }
+            }
+        
+        if(counter < 2){
+
+            theResult.textContent = `${counter} copy of the number ${clickedNumber}`;
+
+        }
+
+        if(counter > 1){
+            theResult.textContent = `${counter} copies of the number ${clickedNumber}`;
+        }
+
+        })
+
+    }
+
+    resetButton.addEventListener("click", function(){
+
+        for(let box of boxes){
+
+            box.style.backgroundColor = "lightgray";
+        }
+
+        theResult.textContent = `Click on a number to find copies`;
+    })
+
+
+}
+
+findNumber();
+
+
+numButton.addEventListener("click", function(){
+    findNumber();
+})
