87b4e7fafe270139be4dd36a21d472f6152e9a38
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2025-01-08 16:31:36 +0100

    Clear finished
    I had to check over my eventlisteners multiple times because I couldn't make them work a second time. I also made the mistake of putting an addeventlistener inside of mosueover which caused it to add multiple ones. Now I only have only of each addeventlistener with different If's depending on how the box looks. Much easier to control.

--- clear/index.js
+++ clear/index.js
@@ -3,50 +3,62 @@ document.title = "Numbers:: Clear";
 let theButton = document.getElementById("fillClear");
 
 
-function clear(){
+function randomizer(){
+
+   return Math.floor(Math.random() * 100);
+}
+
+function clear() {
     const boxes = document.querySelectorAll('.numberBox');
 
-    for(let box of boxes){
-
-        box.addEventListener("mouseover", function(){
-
-            if(box.style.color != "red" && box.style.color != "orange"){
-
+    for (let box of boxes) {
+        box.addEventListener("mouseover", function() {
+            if (box.style.backgroundColor === "red") {
+                box.style.color = "orange";
+                box.style.backgroundColor = "orange";
+            } else {
                 box.style.backgroundColor = "blue";
-
-            };
-
-            box.addEventListener("click", function(){
-
-                box.style.color = "red";
-                box.style.backgroundColor = "red";
-            });
+            }
         });
 
-        box.addEventListener("mouseout", function(){
-            if(box.style.color == "red"){
-                box.style.backgroundColor = "orange";
-                box.style.color = "orange";
+        box.addEventListener("mouseout", function() {
+            if (box.style.backgroundColor === "orange") {
+                box.style.backgroundColor = "red";
+                box.style.color = "red";
+            } else {
+                box.style.backgroundColor = "lightgray";
             }
+        });
 
-            if(box.style.color != "orange"){
-                box.style.backgroundColor ="lightgray";
+        box.addEventListener("click", function() {
+            if (box.style.backgroundColor === "blue") {
+                box.style.color = "orange";
+                box.style.backgroundColor = "orange";
+            } else if (box.style.backgroundColor === "orange") {
+                box.style.backgroundColor = "blue";
+                box.textContent = `${randomizer()}`;
+                box.style.color = "black";
             }
-        })
+        });
     }
 
-    theButton.addEventListener("click", function(){
-
-        for(let box of boxes){
-
-            box.style.backgroundColor = "lightgray";
-            box.style.color = "black";
+    theButton.addEventListener("click", function() {
+        for (let box of boxes) {
+            if (box.style.color === "red") {
+                box.style.color = "black";
+                box.textContent = `${randomizer()}`;
+                box.style.backgroundColor = "lightgray";
+            }
         }
-    })
+    });
 }
 
 clear();
 
+
+
+
 numButton.addEventListener("click", function(){
     clear();
+
 });
